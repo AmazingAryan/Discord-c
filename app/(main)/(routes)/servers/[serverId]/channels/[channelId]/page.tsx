9431efd33aceb9e0e7366a1ca65e4bfac812abbd
Aryan <aryan191990@gmail.com>
2024-08-07 23:12:17 +0530

    Send messages, edit messages

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,67 +1,73 @@
 import { ChatHeader } from "@/components/chat/chat-header";
 import { ChatInput } from "@/components/chat/chat-input";
+import { ChatMessages } from "@/components/chat/chat-messages";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { redirectToSignIn } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
-
 interface ChannelIdPageprops {
-    params: {
-        serverId: string;
-        channelId: string;
-    }
+  params: {
+    serverId: string;
+    channelId: string;
+  };
 }
 
-const ChannelIdPage = async ({
-    params
-}: ChannelIdPageprops) => {
+const ChannelIdPage = async ({ params }: ChannelIdPageprops) => {
+  const profile = await currentProfile();
+  if (!profile) {
+    return redirectToSignIn();
+  }
 
-    const profile = await currentProfile();
-    if(!profile){
-        return redirectToSignIn();
-    }
+  const channel = await db.channel.findUnique({
+    where: {
+      id: params.channelId,
+    },
+  });
 
-    const channel = await db.channel.findUnique({
-        where: {
-            id: params.channelId,
-        },
-    });
+  const member = await db.member.findFirst({
+    where: {
+      serverId: params.serverId,
+      profileId: profile?.id,
+    },
+  });
 
-    const member = await db.member.findFirst({
-        where: {
-            serverId: params.serverId,
-            profileId: profile?.id,
-        }
-    });
+  if (!channel || !member) {
+    redirect("/");
+  }
 
-    if(!channel || !member){
-        redirect("/");
-    }
-   
-    return (
-        <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
-            <ChatHeader
-                name={channel.name}
-                serverId={channel.serverId}
-                type="channel"
-            
-            />
-            <div className="flex-1"> Future messages</div>
-            <ChatInput
-                name={channel.name}
-                type="channel"
-                apiUrl="/api/socket/messages"
-                query={{
-                    channelId: channel.id,
-                    serverId: channel.serverId,
-                }}
-
-            />
-        </div>
-    );
-
-    
-}
+  return (
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+      <ChatHeader
+        name={channel.name}
+        serverId={channel.serverId}
+        type="channel"
+      />
+      <ChatMessages
+        name={channel.name}
+        member={member}
+        chatId={channel.id}
+        apiUrl={"/api/messages"}
+        socketUrl={"/api/socket/messages"}
+        socketQuery={{
+          channelId: channel.id,
+          serverId: channel.serverId,
+        }}
+        paramKey={"channelId"}
+        paramValue={channel.id}
+        type={"channel"}
+      />
+      <ChatInput
+        name={channel.name}
+        type="channel"
+        apiUrl="/api/socket/messages"
+        query={{
+          channelId: channel.id,
+          serverId: channel.serverId,
+        }}
+      />
+    </div>
+  );
+};
 
 export default ChannelIdPage;
--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -13,8 +13,11 @@ import {
     FormItem
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Plus, Smile } from "lucide-react";
+import { Plus } from "lucide-react";
 import { useModal } from "@/hooks/use-modal-store";
+import { EmojiPicker } from "@/components/emoji-picker";
+import { useRouter } from "next/navigation";
+
 
 interface ChatInputProps {
     apiUrl: string;
@@ -33,7 +36,9 @@ export const ChatInput = ({
     name,
     type
 }: ChatInputProps) => {
+
     const {onOpen} = useModal();
+    const router = useRouter();
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
@@ -51,6 +56,8 @@ export const ChatInput = ({
             });
 
             await axios.post(url, values);
+            form.reset();
+            router.refresh();
         } catch (error) {
             console.log(error);
         }
@@ -75,7 +82,9 @@ export const ChatInput = ({
                                         {...field}
                                     />
                                     <div className="absolute top-7 right-8">
-                                        <Smile/>
+                                        <EmojiPicker 
+                                            onChange={(emoji: string) => field.onChange(`${field.value} ${emoji}`)}
+                                        />
                                     </div>
                                 </div>
                             </FormControl>
